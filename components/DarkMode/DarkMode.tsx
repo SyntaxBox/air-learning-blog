a98e24d96e15ff057cbb8a2598ba27abc1ef5623
Abdelhamid Boudjit <qhamid@proton.me>
2024-06-12 12:43:27 +0100

    refactor : fix darkmode hook module issue

--- components/DarkMode/DarkMode.tsx
+++ components/DarkMode/DarkMode.tsx
@@ -1,23 +1,25 @@
-import { useTheme } from 'next-themes'
-import { useState, useEffect } from 'react'
-import { DarkModeSwitch } from 'react-toggle-dark-mode'
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 function DarkModeButton() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
- const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
-
-    useEffect(() => {
-    setMounted(true)
-  }, [])
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   if (!mounted) {
-    return null
+    return null;
   }
 
   return (
-            <DarkModeSwitch checked={theme=== "dark"} onChange={() => setTheme(theme=== "light"? "dark":"light")} size={22} sunColor="#374151" />
-
- 
+    <DarkModeSwitch
+      checked={theme === "dark"}
+      onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+      size={22}
+      sunColor="#374151"
+    />
   );
 }
 
--- hooks/useDarkMode.tsx
+++ hooks/useDarkMode.tsx
@@ -1,14 +1,14 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-// export default function useDarkSide() {
-//   const [theme, setTheme] = useState<string>("light");
-//   const colorTheme = theme === "dark" ? "light" : "dark";
-//   useEffect(() => {
-//       const root = window.document.documentElement;
-//       root.classList.remove(colorTheme);
-//       root.classList.add(theme);
-//       localStorage.setItem("theme", theme);
-//   }, [theme, colorTheme]);
+export default function useDarkSide() {
+  const [theme, setTheme] = useState<string>("light");
+  const colorTheme = theme === "dark" ? "light" : "dark";
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme, colorTheme]);
 
-//   return [colorTheme, setTheme];
-// }
+  return [colorTheme, setTheme];
+}
