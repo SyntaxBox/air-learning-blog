eaba2a493094137d828c6801cbc644beeaeeda18
abdelhamid Boudjit <qhamid@proton.me>
2024-06-18 12:56:27 +0100

    Update NavLinks.tsx

--- components/NavLinks/NavLinks.tsx
+++ components/NavLinks/NavLinks.tsx
@@ -1,3 +1,4 @@
+import { Github } from "react-bootstrap-icons";
 function NavLinks() {
   return (
     <div className="w-full">
@@ -6,15 +7,6 @@ function NavLinks() {
         id="navbar-cta"
       >
         <ul className="flex flex-col gap-2 p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-          <li>
-            <a
-              href="#"
-              className="block py-2 pl-3 pr-4 text-white bg-green-500 rounded md:bg-transparent md:text-green-500 md:p-0"
-              aria-current="page"
-            >
-              Home
-            </a>
-          </li>
           <li>
             <a
               href="#"
@@ -37,6 +29,14 @@ function NavLinks() {
               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500 md:p-0"
             >
               Contact
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500 md:p-0 flex items-center gap-2"
+            >
+              <Github /> Repo
             </a>
           </li>
         </ul>
